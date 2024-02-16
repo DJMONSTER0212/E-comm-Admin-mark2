2d37416d81c9462b449858ddd87d4aed55a59a90
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2024-02-17 00:41:45 +0530

    created the folder and route structure

---
+++ app/(pages)/(admin)/layout.tsx
@@ -0,0 +1,16 @@
+
+import { redirect } from "next/navigation"
+import React from "react";
+
+export default async function SetupLayout({
+    children
+}:{
+    children : React.ReactNode;
+}){
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
